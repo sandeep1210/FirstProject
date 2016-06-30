7b7d6d82024082deeb84a3afe616e9ed4214f949
sandeep1210 <sandeep.akinepalli@gmail.com>
2016-06-30 18:56:28 -0500

    Create InterfacesDemo.java

---
+++ InterfacesDemo.java
@@ -0,0 +1,16 @@
+package com.veritis.j2se.DemoProject;
+
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		
+		subclass obj= new subclass();
+		 
+		obj.addition(30,10);
+		obj.subtraction();
+		obj.multiplication();
+		obj.modulus();
+
+	}
+
+}
